e86c31c732dd68097b436d705dc0c6185162486d
ErajSattori <eraj.sattori@gmail.com>
2016-11-23 14:35:20 +0100

    Source Code Added

---
+++ Assets/Scripts/musicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicPlayer : MonoBehaviour {
+	static musicPlayer instance = null;
+	// Use this for initialization
+
+	void Awake () {
+		Debug.Log("Music player Awake." + GetInstanceID());
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate is destroyed!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	void Start ()
+	{  
+		Debug.Log("Music player Start." + GetInstanceID());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/playerControl.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+	public KeyCode upKey;
+	public KeyCode downKey;
+	public KeyCode leftKey;
+	public KeyCode rightKey;
+
+	public float speed = 16;
+	public GameObject wallPrefab;
+
+	Collider2D wall;
+	Vector2 lastWall;
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+		spawnWall();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(upKey)) {
+			GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+			spawnWall();
+		}
+		else if (Input.GetKeyDown(downKey)) {
+			GetComponent<Rigidbody2D>().velocity = -Vector2.up * speed;
+			spawnWall();
+		}
+		else if (Input.GetKeyDown(rightKey)) {
+			GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+			spawnWall();
+		}
+		else if (Input.GetKeyDown(leftKey)) {
+			GetComponent<Rigidbody2D>().velocity = -Vector2.right * speed;
+			spawnWall();
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (Time.timeScale == 1.0F)
+				Time.timeScale = 0.7F;
+			else
+				Time.timeScale = 1.0F;
+			Time.fixedDeltaTime = 0.02F * Time.timeScale;
+		}
+
+		fitColliderBetween(wall, lastWall, transform.position);
+	}
+	void fitColliderBetween(Collider2D collision, Vector2 a, Vector2 b) {
+		
+		collision.transform.position = a + (b - a) * 0.5f;
+
+		float dist = Vector2.Distance(a, b);
+		if (a.x != b.x)
+			collision.transform.localScale = new Vector2(dist + 1, 1);
+		else
+			collision.transform.localScale = new Vector2(1, dist + 1);
+	}
+
+	void spawnWall()
+	{
+		lastWall = transform.position;
+
+		GameObject g = (GameObject)Instantiate(wallPrefab, transform.position, Quaternion.identity);
+		wall = g.GetComponent<Collider2D>();
+	}
+
+	void OnTriggerEnter2D(Collider2D co) {
+		if (co != wall) {
+			if (this.tag == "Player2") {
+				Destroy (gameObject);
+				print ("Player 1 Won");
+				Application.LoadLevel ("Player1win");
+			} else if (this.tag == "Player") {
+				Destroy (gameObject);
+				print ("Player 2 won");
+				Application.LoadLevel ("Player2win");
+			}
+		}
+	}
+}
